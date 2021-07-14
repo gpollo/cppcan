c0f90f865c6520acd214066bfda6b278bc1d6aad
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2021-07-14 13:18:49 -0400

    Add can::database::dbc::ast::requirements class

---
+++ include/can/database/dbc/ast/requirements.hpp
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_CAN_DATABASE_DBC_AST_REQUIREMENTS_HPP
+#define INCLUDE_CAN_DATABASE_DBC_AST_REQUIREMENTS_HPP
+
+#include <string>
+#include <vector>
+
+namespace can::database::dbc::ast {
+
+using requirements = std::vector<std::string>;
+
+} /* namespace can::database::dbc::ast */
+
+#endif /* INCLUDE_CAN_DATABASE_DBC_AST_REQUIREMENTS_HPP */
