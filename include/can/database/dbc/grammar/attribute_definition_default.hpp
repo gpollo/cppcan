377fb83dc7b601d7615a1a542c3caf3fd5af5ba6
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2021-07-14 13:02:01 -0400

    Add can::database::dbc::ast::attribute_definition_default class

---
+++ include/can/database/dbc/grammar/attribute_definition_default.hpp
@@ -0,0 +1,39 @@
+#ifndef INCLUDE_CAN_DATABASE_DBC_GRAMMAR_ATTRIBUTE_DEFINITION_DEFAULT_HPP
+#define INCLUDE_CAN_DATABASE_DBC_GRAMMAR_ATTRIBUTE_DEFINITION_DEFAULT_HPP
+
+#include "can/database/dbc/ast/attribute_definition_default.hpp"
+#include "can/database/dbc/grammar/attribute.hpp"
+
+namespace can::database::dbc::grammar {
+
+struct attribute_definition_default {
+    static constexpr auto rule_prefix = LEXY_LIT("BA_DEF_DEF_");
+
+    static constexpr auto rule_whitespace = lexy::dsl::whitespace(lexy::dsl::ascii::blank);
+
+    struct name {
+        static constexpr auto rule_chars = lexy::dsl::ascii::alpha_digit_underscore / lexy::dsl::ascii::space;
+
+        static constexpr auto rule = lexy::dsl::quoted(rule_chars);
+
+        static constexpr auto value = lexy::as_string<std::string>;
+    };
+
+    static constexpr auto rule_name = lexy::dsl::p<name>;
+
+    static constexpr auto rule_text = lexy::dsl::p<attribute::text>;
+
+    static constexpr auto rule_semicolon = lexy::dsl::semicolon;
+
+    static constexpr auto rule_eol = lexy::dsl::eol;
+
+    static constexpr auto rule = lexy::dsl::peek(rule_prefix) >>
+                                 (rule_prefix + rule_whitespace + rule_name + rule_whitespace + rule_text +
+                                  rule_whitespace + rule_semicolon + rule_whitespace + rule_eol);
+
+    static constexpr auto value = lexy::construct<ast::attribute_definition_default>;
+};
+
+} /* namespace can::database::dbc::grammar */
+
+#endif /* INCLUDE_CAN_DATABASE_DBC_GRAMMAR_ATTRIBUTE_DEFINITION_DEFAULT_HPP */
